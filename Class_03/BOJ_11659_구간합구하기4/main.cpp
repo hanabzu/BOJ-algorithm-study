22106cada940673b256e5fcd1a65309876d6a427
hanabzu <hanthird@gmail.com>
2021-06-21 22:04:24 +0900

    Create: BOJ_11659_구간합구하기4

---
+++ Class_03/BOJ_11659_구간합구하기4/main.cpp
@@ -0,0 +1,32 @@
+﻿/* hanabzu */
+/* BOJ_11659 구간 합 구하기 4 */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M, i, j;
+	int* num;
+
+	cin >> N >> M;
+
+	num = new int[N+1];
+	num[0] = 0;
+
+	for (int k = 1; k < N+1; k++) {
+		cin >> num[k];
+		num[k] += num[k - 1];
+	}
+
+	for (int k = 0; k < M; k++) {
+		cin >> i >> j;
+		cout << num[j] - num[--i] << '\n';
+	}
+
+	return 0;
+}
