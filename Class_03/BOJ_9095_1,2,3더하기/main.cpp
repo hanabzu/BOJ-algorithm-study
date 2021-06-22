c5244758aa36ea750ffc2f985d2f8a89aca34322
hanabzu <hanthird@gmail.com>
2021-06-23 04:25:19 +0900

    Create: BOJ_9095_1,2,3 더하기

---
+++ Class_03/BOJ_9095_1,2,3더하기/main.cpp
@@ -0,0 +1,36 @@
+﻿/* hanabzu */
+/* BOJ_9095 1,2,3 더하기 */
+
+#include <iostream>
+using namespace std;
+
+int memo[11] = { 0, };
+
+int count(int n);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t, n;
+	cin >> t;
+
+	memo[0] = 1;
+	memo[1] = 2;
+	memo[2] = 4;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		cout << count(n - 1) << '\n';
+	}
+
+	return 0;
+}
+
+int count(int n) {
+	if (memo[n] != 0) {
+		return memo[n];
+	}
+
+	memo[n] = count(n - 1) + count(n - 2) + count(n - 3);
+	return memo[n];
+}
---
+++ Class_03/BOJ_9095_1,2,3더하기/readme.md
@@ -0,0 +1,6 @@
+### BOJ_9095 1,2,3 더하기
+
+memoization 배열 사용하여 dynamic programming.
+
+정수 n을 1,2,3의 합으로 나타내는 경우의 수는 n-1, n-2, n-3의 경우의 수의 합임.
+
