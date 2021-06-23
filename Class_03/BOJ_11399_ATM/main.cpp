e048057f4e4d5401c65e809ff99ea86db9b6b30d
hanabzu <hanthird@gmail.com>
2021-06-23 14:43:36 +0900

    Create: BOJ_11399_ATM

---
+++ Class_03/BOJ_11399_ATM/main.cpp
@@ -0,0 +1,30 @@
+﻿/* hanabzu */
+/* BOJ_11399 ATM */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, t, total;
+	cin >> n;
+	int time[1000];
+
+	for (int i = 0; i < n; i++) {
+		cin >> time[i];
+	}
+
+	sort(time, time + n);
+
+	total = time[0];
+	for (int i = 1; i < n; i++) {
+		time[i] += time[i-1];
+		total += time[i];
+	}
+
+	cout << total;
+}
---
+++ Class_03/BOJ_11399_ATM/readme.md
@@ -0,0 +1,4 @@
+### BOJ_11399 ATM
+
+greedy algorithm으로 입력받은 time들을 sort한 후 누적합의 summation을 구함.
+
