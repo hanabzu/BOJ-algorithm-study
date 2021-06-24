9f33b8816e1bba1703c6ba796e947c8221ed7788
hanabzu <hanthird@gmail.com>
2021-06-24 15:52:16 +0900

    Create: BOJ_1541_잃어버린괄호

---
+++ Class_03/BOJ_1541_잃어버린괄호/main.cpp
@@ -0,0 +1,43 @@
+﻿/* hanabzu */
+/* BOJ_1541 잃어버린 괄호 */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string input, token = "";
+	int result = 0;
+	bool minus = false;
+
+	cin >> input;
+
+	for (int i = 0; i < input.size()+1; i++) {
+		if (isdigit(input[i])) {
+			token += input[i];
+		}
+		else {
+			if (minus) {
+				result -= stoi(token);
+			}
+			else { // minus == false
+				if(input[i] == '-') {
+					minus = true;
+				}
+				result += stoi(token);
+			}
+			token = "";
+		}
+	}
+
+	cout << result;
+
+	return 0;
+
+}
