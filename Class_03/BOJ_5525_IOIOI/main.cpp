41e225e8f24fc05d2ba3d96e03a229fe64dd25c1
hanabzu <hanthird@gmail.com>
2021-06-26 02:46:19 +0900

    Create: BOJ_5525_IOIOI

---
+++ Class_03/BOJ_5525_IOIOI/main.cpp
@@ -0,0 +1,55 @@
+﻿/* hanabzu */
+/* BOJ_5525 IOIOI */
+
+#include <iostream>
+using namespace std;
+
+char t;
+int len = 0, stmt = 0;
+int n, m, result = 0;
+string input;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> n >> m >> input;
+	for (int i = 0; i < m + 1; i++) {
+		t = input[i];
+		if (stmt == 0) {
+			if (t == 'I') {
+				stmt = 1;
+			}
+		}
+		else if (stmt == 1) {
+			if (t == 'O') {
+				stmt = 2;
+			}
+			else {
+				if (len >= n) {
+					result += len - n + 1;
+				}
+				len = 0;
+				stmt = 1;
+			}
+		}
+		else if (stmt == 2) {
+			if (t == 'I') {
+				len++;
+				stmt = 1;
+			}
+			else {
+				if (len >= n) {
+					result += len - n + 1;
+				}
+				len = 0;
+				stmt = 0;
+			}
+		}
+	}
+
+	cout << result;
+
+	return 0;
+}
---
+++ Class_03/BOJ_5525_IOIOI/readme.md
@@ -0,0 +1,9 @@
+### BOJ_5525 IOIOI
+
+finite automata로 해결함.
+
+statement는 총 3개로, final statement 는 변수 `stmt = 1 or 2`일 때임.
+
+간단하게 문자열을 모두 순회하며 문자를 하나씩 보고 statement를 if문으로 옮겨가며 IOI(OI)*의 길이를 len에 저장하고, 만약 reject가 나면 result에 `len - n  + 1`을 더한 뒤 statement를 옮기고 len을 0으로 초기화 한 후 이를 계속 반복함.
+
+처음에 틀렸던 이유는 단순히 for문에서 조건을 `i < m`으로 했기 때문으로, `i < m + 1`로 바꾸여 해결함.
