e572cda33bd72d21b6ab58203f8f23c921943d97
hanabzu <hanthird@gmail.com>
2021-06-26 15:13:26 +0900

    Create: BOJ_11047_동전0

---
+++ Class_03/BOJ_11047_동전0/main.cpp
@@ -0,0 +1,36 @@
+﻿/* hanabzu */
+/* BOJ_11047 동전 0 */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, K, result = 0;
+	int coin[10];
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		cin >> coin[i];
+	}
+
+	N--;
+	while (K > 0) {
+		if (K >= coin[N]) {
+			K -= coin[N];
+			result++;
+		}
+		else {
+			N--;
+		}
+
+	}
+
+	cout << result;
+
+	return 0;
+}
---
+++ Class_03/BOJ_11047_동전0/readme.md
@@ -0,0 +1,4 @@
+### BOJ_11047 동전 0
+
+greedy algorithm으로 해결.
+제일 큰 동전부터 K 에서 빼가면서 result를 구함.
