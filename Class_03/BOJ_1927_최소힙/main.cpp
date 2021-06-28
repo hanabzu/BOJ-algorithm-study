148ae0bf98b30e32760634cf3c296f12d905446b
hanabzu <hanthird@gmail.com>
2021-06-29 00:07:52 +0900

    Create: BOJ_1927_최소힙

---
+++ Class_03/BOJ_1927_최소힙/main.cpp
@@ -0,0 +1,34 @@
+﻿/* hanabzu */
+/* BOJ_1927 최소 힙 */
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, x;
+	priority_queue<int, vector<int>, greater<int>> q;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		if (x == 0) {
+			if (q.empty()) {
+				cout << 0 << '\n';
+			}
+			else {
+				cout << q.top() << '\n';
+				q.pop();
+			}
+		}
+		else {
+			q.push(x);
+		}
+	}
+	return 0;
+}
---
+++ Class_03/BOJ_1927_최소힙/readme.md
@@ -0,0 +1,3 @@
+### BOJ_1927 최소 힙
+
+`priority_queue<int, vector<int>, greater<int>>`로 선언하면 min heap이 됨.
