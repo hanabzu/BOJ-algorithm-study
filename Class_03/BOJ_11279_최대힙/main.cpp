7aae643d3c20128f33d6786ba44f90b7325120fe
hanabzu <hanthird@gmail.com>
2021-06-28 23:49:20 +0900

    Create: BOJ_11279_최대힙

---
+++ Class_03/BOJ_11279_최대힙/main.cpp
@@ -0,0 +1,35 @@
+﻿/* hanabzu */
+/* BOJ_11279 최대 힙 */
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, x;
+	priority_queue<int, vector<int>> q;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		if (x == 0) {
+			if (q.empty()) {
+				cout << 0 << '\n';
+			}
+			else {
+				cout << q.top() << '\n';
+				q.pop();
+			}
+		}
+		else {
+			q.push(x);
+		}
+	}
+	return 0;
+}
---
+++ Class_03/BOJ_11279_최대힙/readme.md
@@ -0,0 +1,5 @@
+### BOJ_11279 최대 힙
+
+priority queue를 사용해서 구현.
+
+`priority_queue<int, vector<int>>`로 선언하면 max heap이 됨.
