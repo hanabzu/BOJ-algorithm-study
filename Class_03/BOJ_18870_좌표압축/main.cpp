ed3490cb3d257bf7490df840d4fae1555428d68a
hanabzu <hanthird@gmail.com>
2021-06-29 21:43:16 +0900

    Create: BOJ_18870 좌표 압축

---
+++ Class_03/BOJ_18870_좌표압축/main.cpp
@@ -0,0 +1,37 @@
+﻿/* hanabzu */
+/* BOJ_18870 좌표 압축 */
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int N, n;
+vector<int> nums, sorted_nums;
+set<int> s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> n;
+		nums.push_back(n);
+		s.insert(n);
+	}
+
+	for (set<int>::iterator iter = s.begin(); iter != s.end(); iter++) {
+		sorted_nums.push_back(*iter);
+	}
+
+ 	for (int i = 0; i < N; i++) {
+		vector<int>::iterator iter = lower_bound(sorted_nums.begin(), sorted_nums.end(), nums[i]);
+		cout << iter - sorted_nums.begin() << ' ';
+	}
+
+	return 0;
+}
