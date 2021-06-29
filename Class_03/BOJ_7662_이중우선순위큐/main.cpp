bd7e7287a3497c2a4ff7d3f0c4b135f8d3923e76
hanabzu <hanthird@gmail.com>
2021-06-30 02:21:10 +0900

    Create: BOJ_7662 이중 우선순위 큐

---
+++ Class_03/BOJ_7662_이중우선순위큐/main.cpp
@@ -0,0 +1,54 @@
+﻿/* hanabzu */
+/* BOJ_7662 이중 우선순위 큐 */
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int T, k, n;
+char c;
+
+void dpq();
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		dpq();
+	}
+
+}
+
+void dpq() {
+	multiset<int> q;
+
+	cin >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> c >> n;
+		if (c == 'I') {
+			q.insert(n);
+		}
+		else { // c == 'D'
+			if (q.empty())
+				continue;
+
+			if (n == -1) {
+				q.erase(q.begin());
+			}
+			else { // n == 1
+				q.erase(--q.end());
+			}
+		}
+	}
+
+	if (q.empty()) {
+		cout << "EMPTY" << '\n';
+	}
+	else {
+		cout << *(--q.end()) << ' ' << *q.begin() << '\n';
+	}
+}
---
+++ Class_03/BOJ_7662_이중우선순위큐/readme.md
@@ -0,0 +1,8 @@
+### BOJ_7662 이중 우선순위 큐
+
+multiset 사용하여 구현. (`multiset<int> q;`)
+
+D -1인 경우 `q.erase(q.begin());`, D 1인 경우 `q.erase(--q.end());`로 큐에서 삭제함.
+
+
+
