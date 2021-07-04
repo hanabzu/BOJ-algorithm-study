e97c0f1151f1da7b2536a2038e2ef74091fd0a43
hanabzu <hanthird@gmail.com>
2021-07-04 23:15:50 +0900

    Create: BOJ_11053 가장 긴 증가하는 부분 수열

---
+++ Class_04/BOJ_11053_가장긴증가하는부분수열/main.cpp
@@ -0,0 +1,33 @@
+﻿/* hanabzu */
+/* BOJ_11053 가장 긴 증가하는 부분 수열 */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int A[1000];
+int len[1000];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+	for (int i = 0; i < N; i++) {
+		len[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (A[j] < A[i]) {
+				len[i] = max(len[i], len[j] + 1);
+			}
+		}
+	}
+	cout << *max_element(len, len + N);
+
+	return 0;
+}
