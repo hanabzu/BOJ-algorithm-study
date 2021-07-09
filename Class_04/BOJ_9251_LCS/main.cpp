39d9733cdb9b0e5ace3be9974f77169e20bafcf6
hanabzu <hanthird@gmail.com>
2021-07-09 15:47:56 +0900

    Create: BOJ_9251 LCS

---
+++ Class_04/BOJ_9251_LCS/main.cpp
@@ -0,0 +1,35 @@
+﻿/* hanabzu */
+/* BOJ_9251 LCS */
+
+#include <iostream>
+using namespace std;
+
+string X, Y;
+int m, n;
+int c[1001][1001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> X >> Y;
+	m = X.length();
+	n = Y.length();
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (X[i] == Y[j]) {
+				c[i+1][j+1] = c[i][j] + 1;
+			}
+			else if (c[i][j + 1] >= c[i + 1][j]) {
+				c[i+1][j+1] = c[i][j+1];
+			}
+			else {
+				c[i+1][j+1] = c[i+1][j];
+			}
+		}
+	}
+
+	cout << c[m][n];
+	return 0;
+}
