132a5961efda5be11e93168f7079f0cf17c7af11
hanabzu <hanthird@gmail.com>
2022-01-16 22:17:11 +0900

    Create: BOJ_10828 스택

---
+++ SDS/자료구조/BOJ_10828_스택/main.cpp
@@ -0,0 +1,56 @@
+﻿/* BOJ_10828 스택 */
+/* hanabzu */
+
+#include <iostream>
+#define MAXN 100000
+
+using namespace std;
+
+string s;
+int N, num, top = -1;
+int stack[MAXN + 1];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	while (N--) {
+		cin >> s;
+		if (s == "push") {
+			cin >> stack[++top];
+		}
+		else if (s == "pop") {
+			if (top < 0) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << stack[top--] << '\n';
+			}
+		}
+		else if (s == "size") {
+			cout << top + 1 << '\n';
+		}
+		else if (s == "empty") {
+			if (top < 0) {
+				cout << 1 << '\n';
+			}
+			else {
+				cout << 0 << '\n';
+			}
+		}
+		else { // s == "top"
+			if (top < 0) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << stack[top] << '\n';
+			}
+		}
+	}
+
+
+	return 0;
+}
