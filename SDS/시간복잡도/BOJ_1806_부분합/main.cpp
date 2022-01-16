1da8a9ab3fd7e4f3c235bd6a1760f0c1b94bbd82
hanabzu <hanthird@gmail.com>
2022-01-16 22:03:19 +0900

    Create: BOJ_1806 부분합

---
+++ SDS/시간복잡도/BOJ_1806_부분합/main.cpp
@@ -0,0 +1,32 @@
+﻿/* BOJ_1806 부분합 */
+/* hanabzu */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int num[100001];
+int N, S ;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N >> S;
+	for (int i = 0; i < N; i++) {
+		cin >> num[i];
+	}
+
+	int left = 0, right = 0, sum = 0, ans = N + 1;
+
+	while (right <= N) {
+		if (sum >= S) {
+			ans = min(ans, right - left);
+			sum -= num[left++];
+		}
+		else {
+			sum += num[right++];
+		}
+	}
+	cout << ((ans == N + 1) ? 0 : ans);
+
+	return 0;
+}
