87c4907f76343838b037afee211d3f1f8d2393fe
hanabzu <hanthird@gmail.com>
2022-01-16 22:03:59 +0900

    Create: BOJ_2748 피보나치 수 2

---
+++ SDS/시간복잡도/BOJ_2748_피보나치수2/main.cpp
@@ -0,0 +1,33 @@
+﻿/* BOJ_2748 피보나치 수 2 */
+/* hanabzu */
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+long long mem[91];
+
+long long fib(int n);
+
+int main() {
+	cin >> n;
+	mem[1] = 1;
+	cout << fib(n);
+
+	return 0;
+}
+
+
+long long fib(int n) {
+	if (n == 0) {
+		return 0;
+	}
+	if (mem[n] > 0) {
+		return mem[n];
+	}
+
+	mem[n] += fib(n - 1) + fib(n - 2);
+	return mem[n];
+
+}
