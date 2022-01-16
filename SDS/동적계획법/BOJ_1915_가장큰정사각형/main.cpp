138fc5a67240587a98b2eedfbe17eb902a6c55a8
hanabzu <hanthird@gmail.com>
2022-01-16 23:07:03 +0900

    Create: BOJ_1915 가장 큰 정사각형

---
+++ SDS/동적계획법/BOJ_1915_가장큰정사각형/main.cpp
@@ -0,0 +1,28 @@
+﻿/* BOJ_1915 가장 큰 정사각형 */
+/* hanabzu */
+
+#include <iostream>
+#include <algorithm>
+#define MAXN 1000
+using namespace std;
+
+int n, m, res = 0;
+int dp[MAXN + 1][MAXN + 1];
+string s;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> s;
+		for (int j = 1; j <= m; j++) {
+			if (s[j-1]=='1') {
+				dp[i][j] = min({ dp[i - 1][j], dp[i][j - 1],dp[i - 1][j - 1] }) + 1;
+			}
+			res = max(dp[i][j], res);
+		}
+	}
+	cout << res * res;
+
+	return 0;
+}
