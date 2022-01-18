94ce7b6cf298aa264384c0629fb52c001e0195a2
hanabzu <hanthird@gmail.com>
2022-01-18 13:58:44 +0900

    Create: BOJ_1072 게임

---
+++ SDS/시간복잡도/BOJ_1072_게임/main.cpp
@@ -0,0 +1,39 @@
+﻿/* BOJ_1072 게임 */
+/* hanabzu */
+
+#include <iostream>
+#define MAXX 1000000000
+
+using namespace std;
+long long X, Y, Z;
+int l = 0, r = MAXX, m;
+bool isChange(int m);
+
+int main() {
+	cin >> X >> Y;
+	Z = Y*100 / X;
+	if (Z > 98) {
+		cout << -1;
+	}
+	else {
+		while (l + 1 < r) {
+			m = l + ((r - l) >> 1);
+			if (isChange(m)) {
+				r = m;
+			}
+			else {
+				l = m;
+			}
+		}
+		cout << r;
+	}
+
+	return 0;
+}
+
+bool isChange(int m) {
+	if (Z < (Y + m) * 100 / (X + m))
+		return true;
+	else
+		return false;
+}
---
+++ SDS/시간복잡도/BOJ_1072_게임/readme.md
@@ -0,0 +1,3 @@
+### BOJ_1072 게임
+
+binary search로 구함.
