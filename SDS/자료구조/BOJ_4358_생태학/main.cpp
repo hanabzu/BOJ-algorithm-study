d81392c2dc05d5632facbd53c969662967108940
hanabzu <hanthird@gmail.com>
2022-01-20 14:09:48 +0900

    Create: BOJ_4358 생태학

---
+++ SDS/자료구조/BOJ_4358_생태학/main.cpp
@@ -0,0 +1,38 @@
+﻿/* BOJ_4358 생태학 */
+/* hanabzu */
+
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+float total = 0, n;
+string s;
+map<string, int> m;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (getline(cin,s)) {
+		total++;
+		if (m[s]) {
+			m[s]++;
+		}
+		else {
+			m[s] = 1;
+		}
+	}
+
+	for (map<string, int>::iterator it = m.begin(); it != m.end(); it++) {
+		s = it->first;
+		n = it->second;
+
+		cout << s << ' ';
+		cout << fixed;
+		cout.precision(4);
+		cout << n / total *100 << '\n';
+	}
+
+	return 0;
+}
