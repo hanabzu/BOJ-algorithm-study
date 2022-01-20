93a4f886438cda6700c87073197115d8daaae550
hanabzu <hanthird@gmail.com>
2022-01-20 17:01:16 +0900

    Create: BOJ_1655 가운데를 말해요

---
+++ SDS/자료구조/BOJ_1655_가운데를말해요/main.cpp
@@ -0,0 +1,42 @@
+﻿/* BOJ_1655 가운데를 말해요 */
+/* hanabzu */
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int N, n;
+priority_queue<int> maxq;
+priority_queue<int, vector<int>, greater<int>> minq;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	while (N--) {
+		cin >> n;
+		if (maxq.size()>minq.size()) {
+			minq.push(n);
+		}
+		else {
+			maxq.push(n);
+		}
+
+		if (maxq.size() > 0 && minq.size() > 0) {
+			if (maxq.top() > minq.top()) {
+				int maxtop = maxq.top();
+				int mintop = minq.top();
+				maxq.pop();
+				minq.pop();
+				maxq.push(mintop);
+				minq.push(maxtop);
+			}
+		}
+		cout << maxq.top() << '\n';
+	}
+
+	return 0;
+}
---
+++ SDS/자료구조/BOJ_1655_가운데를말해요/readme.md
@@ -0,0 +1,3 @@
+### BOJ_1655 가운데를 말해요
+
+priority queue 사용해서 구현
