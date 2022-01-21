886758ead9d711cf3ffd1cd1271a65dbeb024322
hanabzu <hanthird@gmail.com>
2022-01-21 21:02:04 +0900

    Create: BOJ_2014 소수의 곱

---
+++ SDS/자료구조/BOJ_2014_소수의곱/main.cpp
@@ -0,0 +1,42 @@
+﻿/* BOJ_2014 소수의 곱 */
+/* hanabzu */
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include "limits.h"
+
+using namespace std;
+int K, N;
+long long v;
+int p[100];
+priority_queue<int, vector<int>, greater<int>> q;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> K >> N;
+
+	for (int i = 0; i < K; i++) {
+		cin >> p[i];
+		q.push(p[i]);
+	}
+
+	while (--N) {
+		int u = q.top();
+		q.pop();
+		for (int i = 0; i < K; i++) {
+			v = (long long)u * p[i];
+			if (v < INT_MAX) {
+				q.push(v);
+			}
+			if (u % p[i] == 0) {
+				break;
+			}
+		}
+	}
+
+	cout << q.top();
+	
+	return 0;
+}
---
+++ SDS/자료구조/BOJ_2014_소수의곱/readme.md
@@ -0,0 +1,3 @@
+### BOJ_2014 소수의 곱
+
+priority queue로 풀이.
