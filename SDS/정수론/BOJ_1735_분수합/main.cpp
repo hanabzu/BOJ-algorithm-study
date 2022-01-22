b86272b6586835df5955a1154afcba512b8fc44c
hanabzu <hanthird@gmail.com>
2022-01-22 21:01:58 +0900

    Create: BOJ_1735 분수 합

---
+++ SDS/정수론/BOJ_1735_분수합/main.cpp
@@ -0,0 +1,23 @@
+﻿/* BOJ_1735 분수 합 */
+/* hanabzu */
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int ax, bx, ay, by, c, rx, ry;
+
+int main() {
+
+	cin >> ay >> ax >> by >> bx;
+
+	ry = ay * bx + by * ax;
+	rx = ax * bx;
+	c = gcd(ry, rx); // c++ 17
+
+	cout << ry / c << ' ' << rx / c;
+
+	return 0;
+}
