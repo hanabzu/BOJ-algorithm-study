b6cd6244640eed57df9a23caa71f7ef844bc9837
hanabzu <hanthird@gmail.com>
2022-01-23 20:04:33 +0900

    Create: BOJ_2960 에라토스테네스의 체

---
+++ SDS/정수론/BOJ_2960_에라토스테네스의체/main.cpp
@@ -0,0 +1,34 @@
+﻿/* BOJ_2960 에라토스테네스의 체 */
+/* hanabzu */
+
+#include <iostream>
+#define MAXN 1000
+
+using namespace std;
+
+int N, K;
+bool visit[MAXN + 1];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> K;
+	while (1) {
+		int s = 2;
+		while (visit[s]) s++;
+		int i = s;
+		while (s <= N) {
+			if (!visit[s]) {
+				visit[s] = true;
+				if (!(--K)) {
+					cout << s;
+					exit(0);
+				}
+			}
+			s += i;
+		}
+	}
+
+	return 0;
+}
