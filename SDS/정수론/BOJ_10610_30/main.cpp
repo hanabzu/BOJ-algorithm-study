7c6ff3f29a7f7a44058bd3f5d87258f800a0a89f
hanabzu <hanthird@gmail.com>
2022-01-25 12:44:17 +0900

    Create: BOJ_10610 30

---
+++ SDS/정수론/BOJ_10610_30/main.cpp
@@ -0,0 +1,26 @@
+﻿/* BOJ_10610 30 */
+/* hanabzu */
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+string n;
+multiset<char> s;
+
+int main() {
+	cin >> n;
+
+	int m = 0;
+	for (int i = 0; i < n.size(); i++) {
+		m += n[i] - '0';
+		s.insert(n[i]);
+	}
+	if (*s.begin() != '0' || m % 3 != 0) cout << -1;
+	else {
+		for (multiset<char>::reverse_iterator it = s.rbegin(); it != s.rend(); it++) {
+			cout << *it;
+		}
+	}
+
+}
