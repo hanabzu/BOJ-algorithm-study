03f462156cf7b5eabbe9a80b5b722cf081da6e45
hanabzu <hanthird@gmail.com>
2022-01-25 12:29:48 +0900

    Create: BOJ_4375 1

---
+++ SDS/정수론/BOJ_4375_1/main.cpp
@@ -0,0 +1,29 @@
+﻿/* BOJ_4375 1 */
+/* hanabzu */
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (cin >> n) {
+		int num = 0;
+		for (int i = 1; i <= n; i++) {
+			num = num * 10 + 1;
+			num %= n; // minus multi of n
+			if (num == 0) {
+				cout << i << '\n';
+				break;
+			}
+		}
+	}
+
+
+	return 0;
+}
