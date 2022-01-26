69b07a4b70b0ecc88dce824920f2d8f031b0e559
hanabzu <hanthird@gmail.com>
2022-01-26 21:13:59 +0900

    Create: BOJ_11050 이항계수 1

---
+++ SDS/조합론/BOJ_11050_이항계수1/main.cpp
@@ -0,0 +1,24 @@
+﻿/* BOJ_11050 이항계수 1 */
+/* hanabzu */
+
+#include <iostream>
+
+using namespace std;
+
+int N, K, ans;
+
+int main() {
+	cin >> N >> K;
+
+	ans = 1;
+	for (int i = N; i > N - K; i--) {
+		ans *= i;
+	}
+	for (int i = 2; i <= K; i++) {
+		ans /= i;
+	}
+
+	cout << ans;
+	return 0;
+}
+
