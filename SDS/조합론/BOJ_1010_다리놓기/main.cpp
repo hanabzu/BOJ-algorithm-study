c1666432c5bcd118c3352818b86210e035abf60f
hanabzu <hanthird@gmail.com>
2022-01-27 06:25:01 +0900

    Create: BOJ_1010 다리 놓기

---
+++ SDS/조합론/BOJ_1010_다리놓기/main.cpp
@@ -0,0 +1,31 @@
+﻿/* BOJ_1010 다리 놓기 */
+/* hanabzu */
+
+#include <iostream>
+#define MAXN 30
+using namespace std;
+
+int t, N, M;
+int dp[MAXN + 1][MAXN + 1];
+int pas(int n, int k);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> t;
+	while (t--) {
+		cin >> N >> M;
+		cout << pas(M, N) << '\n';
+	}
+
+	return 0;
+}
+
+int pas(int n, int k) {
+	if (n == 1 || k == 0 || n == k) return 1;
+	if (dp[n][k] != 0) return dp[n][k];
+
+	return dp[n][k] = pas(n - 1, k - 1) + pas(n - 1, k);
+}
