f2f5905d4daeb76c6dd5fe7fd1e7cb730fe1b144
hanabzu <hanthird@gmail.com>
2022-02-07 23:30:38 +0900

    Create: BOJ_9466 텀 프로젝트

---
+++ Class_05/BOJ_9466_텀프로젝트/main1.cpp
@@ -0,0 +1,52 @@
+﻿/* BOJ_9466 텀 프로젝트 */
+/* hanabzu */
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+const int MAXN = 100000;
+
+int T, n, ans;
+int pick[MAXN + 1];
+int indegree[MAXN + 1];
+void solution();
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+	while (T--) {
+		solution();
+	}
+	return 0;
+}
+
+void solution() {
+	cin >> n;
+
+	fill(indegree, indegree + n + 1, 0);
+	ans = 0;
+	for (int i = 1; i <= n; i++) {
+		cin >> pick[i];
+		indegree[pick[i]]++;
+	}
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		if (indegree[i] == 0) q.push(i);
+	}
+
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+		ans++;
+		int v = pick[u];
+		if (--indegree[v] == 0) {
+			q.push(v);
+		}
+	}
+	cout << ans << '\n';
+}
---
+++ Class_05/BOJ_9466_텀프로젝트/main2.cpp
@@ -0,0 +1,76 @@
+﻿/* BOJ_9466 텀 프로젝트 */
+/* hanabzu */
+
+#include <iostream>
+
+using namespace std;
+
+const int MAXN = 100000;
+
+enum {
+	UNVISITED, VISITED, SUCCESS, FAIL
+};
+int T, n, ans;
+int pick[MAXN + 1];
+int visit[MAXN + 1];
+void solution();
+void dfs(int p);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+	while (T--) {
+		solution();
+	}
+
+
+	return 0;
+}
+
+void solution() {
+	cin >> n;
+
+	fill(visit, visit + n + 1, 0);
+	ans = 0;
+	for (int i = 1; i <= n; i++) {
+		cin >> pick[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (visit[i] == UNVISITED) {
+			dfs(i);
+		}
+	}
+	cout << ans << '\n';
+}
+
+void dfs(int p) {
+	if (visit[pick[p]] == SUCCESS || visit[pick[p]] == FAIL) {
+		visit[p] = FAIL;
+		ans++;
+		return;
+	}
+
+	visit[p] = VISITED;
+
+	if (visit[pick[p]] == VISITED) {
+		visit[pick[p]] = SUCCESS; // team
+		return;
+	}
+
+	dfs(pick[p]);
+
+	if (visit[pick[p]] == VISITED) {
+		visit[pick[p]] = SUCCESS;
+	}
+	else if (visit[pick[p]] == SUCCESS || visit[pick[p]] == FAIL) {
+		visit[p] = FAIL;
+		ans++;
+	}
+	else {
+		visit[p] = UNVISITED;
+	}
+}
