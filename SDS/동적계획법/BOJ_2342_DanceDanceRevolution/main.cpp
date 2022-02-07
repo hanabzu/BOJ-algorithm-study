0ea6e476e80f2cabad7e99f17c3555ec3cdfd790
hanabzu <hanthird@gmail.com>
2022-02-07 19:38:00 +0900

    Create: BOJ_2342 Dance Dance Revolution

---
+++ SDS/동적계획법/BOJ_2342_DanceDanceRevolution/main.cpp
@@ -0,0 +1,57 @@
+﻿/* BOJ_2342 Dance Dance Revolution */
+/* hanabzu */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MAXN = 100000;
+
+int dp[MAXN + 1][5][5], input[MAXN + 1];
+int N = 0;
+
+int dfs(int p, int lhs, int rhs);
+int move(int s, int e);
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+		cin >> input[N];
+		if (input[N] == 0) break;
+		N++;
+	}
+
+	cout << dfs(0, 0, 0);
+	return 0;
+}
+
+int dfs(int p, int lhs, int rhs) {
+	if (input[p] == 0) {
+		return 0;
+	}
+
+	if (dp[p][lhs][rhs] > 0) return dp[p][lhs][rhs];
+
+	int next_lhs = dfs(p + 1, input[p], rhs) + move(lhs, input[p]);
+	int next_rhs = dfs(p + 1, lhs, input[p]) + move(rhs, input[p]);
+
+	return dp[p][lhs][rhs] = min(next_lhs, next_rhs);
+}
+
+int move(int s, int e) {
+	if (s == e) {
+		return 1;
+	}
+	else if (s == 0) {
+		return 2;
+	}
+	else if (((s + e) & 1) == 1) {
+		return 3;
+	}
+	else {
+		return 4;
+	}
+}
---
+++ SDS/동적계획법/BOJ_2342_DanceDanceRevolution/readme.md
@@ -0,0 +1,5 @@
+### BOJ_2342 Dance Dance Revolution
+
+dfs + dp로 구현.
+
+아이디어 생각하는 것이 매우 힘든 문제.
