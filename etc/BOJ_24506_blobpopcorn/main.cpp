4d9e17dfcb696e5b2c5fd35dfd9d5e3690ecd436
hanabzu <hanthird@gmail.com>
2022-02-28 00:22:10 +0900

    Create: BOJ_24506 blobpopcorn

---
+++ etc/BOJ_24506_blobpopcorn/main.cpp
@@ -0,0 +1,117 @@
+﻿/* BOJ_24506 blobpopcorn */
+/* hanabzu */
+
+#include <iostream>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+const int MAXN = 100000;
+const int POWN = 18;
+int N, Q, B, q, x, y, m;
+
+pair<int, int> w_l[1 << POWN]; // lx가 존재하지 않는 x의 개수 LIS
+pair<int, int> w_r[1 << POWN]; // rx가 존재하지 않는 x의 개수 LIS
+
+void init();
+void update(int p, int c);
+int count_num(int p, pair<int, int>(&w)[1 << POWN]);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> Q;
+
+	for (B = 1; B < N; B <<= 1);
+
+	for (int i = B; i < B + N; i++) {
+		cin >> w_l[i].first;
+		w_l[i].second = 1;
+		w_r[(B << 1) + N - i - 1] = w_l[i];
+	}
+
+	init();
+	
+	while (Q--) {
+		cin >> q;
+		if (q == 1) {
+			cin >> x >> y;
+			update(x, y);
+		}
+		else {
+			cout << (N << 1) - w_l[1].second - w_r[1].second << '\n';
+		}
+	}
+
+	return 0;
+}
+
+
+void init() {
+	for (int i = B - 1; i > 0; i--) {
+		if (w_l[i << 1].first < w_l[(i << 1) | 1].first) {
+			w_l[i] = w_l[(i << 1) | 1];
+			m = w_l[i << 1].first;
+			w_l[i].second = w_l[i << 1].second + count_num((i << 1) | 1, w_l);
+		}
+		else {
+			w_l[i] = w_l[i << 1];
+		}
+
+		if (w_r[i << 1].first < w_r[(i << 1) | 1].first) {
+			w_r[i] = w_r[(i << 1) | 1];
+			m = w_r[i << 1].first;
+			w_r[i].second = w_r[i << 1].second + count_num((i << 1) | 1, w_r);
+		}
+		else {
+			w_r[i] = w_r[i << 1];
+		}
+	}
+}
+
+void update(int p, int c) {
+	int lp = p + B - 1;
+	int rp = B + N - p;
+
+	w_l[lp].first = w_r[rp].first = c;
+
+	while (lp >>= 1) {
+		if (w_l[lp << 1].first < w_l[(lp << 1) | 1].first) {
+			w_l[lp] = w_l[(lp << 1) | 1];
+			m = w_l[lp << 1].first;
+			w_l[lp].second = w_l[lp << 1].second + count_num((lp << 1) | 1, w_l);
+		}
+		else {
+			w_l[lp] = w_l[lp << 1];
+		}
+	}
+
+	while (rp >>= 1) {
+		if (w_r[rp << 1].first < w_r[(rp << 1) | 1].first) {
+			w_r[rp] = w_r[(rp << 1) | 1];
+			m = w_r[rp << 1].first;
+			w_r[rp].second = w_r[rp << 1].second + count_num((rp << 1) | 1, w_r);
+		}
+		else {
+			w_r[rp] = w_r[rp << 1];
+		}
+	}
+}
+
+int count_num(int p, pair<int, int>(&w)[1 << POWN]) {
+	int cnt = 0;
+	if (w[p].second == 1) {
+		return (w[p].first > m) ? 1 : 0;
+	}
+	if (w[p << 1].first > m) {
+		cnt += w[p].second - w[p << 1].second;
+		return cnt + count_num(p << 1,  w);
+	}
+	else { // w[p << 1].first < m
+		return cnt + count_num((p << 1)|1, w);
+	}
+	
+}
---
+++ etc/BOJ_24506_blobpopcorn/readme.md
@@ -0,0 +1,7 @@
+### BOJ_24506 blobpopcorn
+
+인덱스 트리로 풀이.
+
+ [블롭컵 해설]([제1회 블롭컵 해설 - Google Slides](https://docs.google.com/presentation/d/1wNCFroWIV962QsUwcpe2fUHjJ_2BYqJ-UGTVhvp0pJ8)) 참고하였음.
+
+`count_num()`함수 구현이 너무 어려웠다...
