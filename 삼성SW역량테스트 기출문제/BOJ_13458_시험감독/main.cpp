046a510a9903ce55c19b055708ae3d471c081e33
hanabzu <hanthird@gmail.com>
2022-03-21 11:15:57 +0900

    Create: BOJ_13458 시험감독

---
+++ 삼성SW역량테스트 기출문제/BOJ_13458_시험감독/main.cpp
@@ -0,0 +1,35 @@
+﻿/* BOJ_13458 시험 감독 */
+/* hanabzu */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MAXN = 1000000;
+int N, A[MAXN], B, C;
+long long ans;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+	cin >> B >> C;
+	ans = N;
+	for (int i = 0; i < N; i++) {
+		A[i] = max(0, A[i] - B);
+		ans += A[i] / C;
+		if (A[i] % C != 0) {
+			ans++;
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
