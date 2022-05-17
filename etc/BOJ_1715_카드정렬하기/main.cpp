2f14816ec4eb8ea7c7ff2357776a5e2db8b1bbc4
hanabzu <hanthird@gmail.com>
2022-05-17 10:53:15 +0900

    Create: BOJ_1715 카드 정렬하기

---
+++ etc/BOJ_1715_카드정렬하기/main.cpp
@@ -0,0 +1,35 @@
+﻿/* BOJ_1715 카드 정렬하기 */
+/* hanabzu */
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int N;
+long long A, B, ans = 0;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+
+	priority_queue<int, vector<int>, greater<int>> q;
+
+	while (N--) {
+		cin >> A;
+		q.push(A);
+	}
+
+	while (q.size() >= 2) {
+		A = q.top();
+		q.pop();
+		B = q.top();
+		q.pop();
+		ans += A + B;
+		q.push(A + B);
+	}
+	cout << ans;
+	return 0;
+}
