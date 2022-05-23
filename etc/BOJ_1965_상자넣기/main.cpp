3532a1065f17d17b7a7dbb19cb079931498f717b
hanabzu <hanthird@gmail.com>
2022-05-24 08:56:25 +0900

    Create: BOJ_1965 상자 넣기

---
+++ etc/BOJ_1965_상자넣기/main.cpp
@@ -0,0 +1,33 @@
+﻿/* BOJ_1965 상자넣기 */
+/* hanabzu */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MAXN = 1000;
+
+int N, A[MAXN + 1], dp[MAXN + 1], ans = 1;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> A[i];
+	}
+	dp[1] = A[1];
+
+	for (int i = 1; i <= N; i++) {
+		if (dp[ans] < A[i]) {
+			dp[++ans] = A[i];
+			continue;
+		}
+		dp[lower_bound(dp + 1, dp + ans + 1, A[i]) - dp] = A[i];
+	}
+	cout << ans;
+	return 0;
+}
