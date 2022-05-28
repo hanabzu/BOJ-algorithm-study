65a3c3be51e7b0fde28fce8a84e1e0fa1b646eee
hanabzu <hanthird@gmail.com>
2022-05-28 20:52:38 +0900

    Create: BOJ_1670 정상 회담 2

---
+++ etc/BOJ_1670_정상회담2/main.cpp
@@ -0,0 +1,35 @@
+﻿/* BOJ_1670 정상 회담 2 */
+/* hanabzu */
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+const int MAXN = 10000;
+const int MOD = 987654321;
+
+int N;
+long long dp[MAXN + 1];
+
+long long get_combi(int n);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	dp[0] = dp[2] = 1;
+	cout << get_combi(N);
+
+	return 0;
+}
+
+
+long long get_combi(int n) {
+	if (dp[n] > 0) return dp[n];
+	for (int i = 2; i <= n; i += 2) {
+		dp[n] = (dp[n] + get_combi(i - 2) * get_combi(n - i)) % MOD;
+	}
+	return dp[n];
+}
