08135c6100d029496a1d527ad329d525606dfdc1
hanabzu <hanthird@gmail.com>
2022-06-12 12:27:43 +0900

    Create: BOJ_11048 이동하기

---
+++ etc/BOJ_11048_이동하기/main.cpp
@@ -0,0 +1,32 @@
+﻿/* BOJ_11048 이동하기 */
+/* hanabzu */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int MAXN = 1000;
+
+int N, M, A[MAXN + 1][MAXN + 1], dp[MAXN + 1][MAXN + 1];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			cin >> A[i][j];
+		}
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			dp[i][j] = A[i][j] + max({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] });
+		}
+	}
+
+	cout << dp[N][M];
+	return 0;
+}
