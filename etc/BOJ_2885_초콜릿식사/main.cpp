c09bfd1d6ddee1f2efc23cd6044dbe48d1ebd5a6
hanabzu <hanthird@gmail.com>
2022-06-19 10:39:59 +0900

    Create: BOJ_2885 초콜릿 식사

---
+++ etc/BOJ_2885_초콜릿식사/main.cpp
@@ -0,0 +1,39 @@
+﻿/* BOJ_2885 초콜릿 식사 */
+/* hanabzu */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int K, choco, cnt = 0;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> K;
+
+	for (choco = 1; choco < K; choco <<= 1);
+
+	int t = choco;
+	
+	if (K != choco) {
+		while (K > 0) {
+			if (K - t >= 0) {
+				K -= t;
+				if (K == 0) break;
+			}
+			t >>= 1;
+			cnt++;
+		}
+	}
+
+	
+
+
+	cout << choco << ' ' << cnt;
+
+	return 0;
+}
