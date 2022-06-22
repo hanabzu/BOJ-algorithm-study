74b0a35eeb089652e0241c41fb37393a4053cf5d
hanabzu <hanthird@gmail.com>
2022-06-22 09:54:16 +0900

    Create: BOJ_1786 찾기

---
+++ Class_06/BOJ_1786_찾기/main.cpp
@@ -0,0 +1,58 @@
+﻿/* BOJ_1786 찾기 */
+/* hanabzu */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string T, P;
+vector<int> pi, ans;
+void getPi();
+void kmp();
+
+int main() {
+	getline(cin, T);
+	getline(cin, P);
+	
+	getPi();
+	kmp();
+	cout << ans.size() << '\n';
+	for (int i = 0; i < ans.size(); i++) {
+		cout << ans[i] + 1 << ' ';
+	}
+
+	return 0;
+}
+
+void getPi() {
+	pi = vector<int>(P.size(), 0);
+
+	int i = 1, j = 0;
+	for (i = 1; i < P.size(); i++) {
+		while (j > 0 && P[i] != P[j]) {
+			j = pi[j - 1];
+		}
+		if (P[i] == P[j]) {
+			pi[i] = ++j;
+		}
+	}
+}
+
+void kmp() {
+	int i = 0, j = 0;
+	for (i = 0; i < T.size(); i++) {
+		while (j > 0 && T[i] != P[j]) {
+			j = pi[j - 1];
+		}
+		if (T[i] == P[j]) {
+			if (j == P.size() - 1) {
+				ans.push_back(i - P.size() + 1);
+				j = pi[j];
+			} 
+			else {
+				j++;
+			}
+		}
+	}
+}
---
+++ Class_06/BOJ_1786_찾기/readme.md
@@ -0,0 +1,9 @@
+### BOJ_1786 찾기
+
+KMP.
+
+[KMP : 문자열 검색 알고리즘 - 멍멍멍](https://bowbowbow.tistory.com/6)
+
+[KMP 문자열 탐색 알고리즘이 동작하는 구체적인 원리](https://injae-kim.github.io/dev/2020/07/23/all-about-kmp-algorithm.html)
+
+참고
